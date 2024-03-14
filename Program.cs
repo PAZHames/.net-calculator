60b96ae99bf355e201b583d3b91662190b355961
PAZHames <phoebehames292@gmail.com>
2024-03-14 17:57:31 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.WriteLine("Number 1:");
+// var str1 = Console.ReadLine();
+// Console.WriteLine("Number 2:");
+// var str2 = Console.ReadLine();
+
+// if (str1 != null && str2 != null)
+// {
+//     var n1 = int.Parse(str1);
+//     var n2 = int.Parse(str2);
+//     Console.WriteLine(n1 + n2);
+// }
+// else
+// {
+//     Console.WriteLine("No dice!");
+// }
+
+Console.WriteLine("Enter expression:");
+var exp = Console.ReadLine();
+string[] elements = exp.Split(' ');
+
+if (exp.Contains(' '))
+{
+    for (int i = 0; i < elements.Length; i++)
+    {
+
+        Console.WriteLine(elements[i]);
+    }
+}
